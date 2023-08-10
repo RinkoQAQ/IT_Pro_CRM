463c9255c6f37f7266c474ad7c74c9035c5d5a39
RinkoQAQ <lyyan3@student.unimelb.edu.au>
2023-08-11 01:14:46 +1000

    Edit and photo error fixed

--- js/script.js
+++ js/script.js
@@ -1,41 +1,74 @@
 $(document).ready(function() {
-    // Check if in edit mode
-    var urlParams = new URLSearchParams(window.location.search);
-    var itemsArray = JSON.parse(localStorage.getItem('itemsArray')) || [];
-    var editedContactIndex = null;
-
-    if(urlParams.has('edit')) {
-        editedContactIndex = localStorage.getItem('editedContactIndex');
-        var editedContact = itemsArray[editedContactIndex];
-        
-        // Pre-fill the form
-        $("input[name='name']").val(editedContact.name);
-        $("input[name='email']").val(editedContact.email);
-        $("input[name='phone']").val(editedContact.phone);
-        $("input[name='birthday']").val(editedContact.birthday);
-        $("input[name='company']").val(editedContact.company);
-        $("select[name='group']").val(editedContact.group);
-        $("textarea[name='notes']").val(editedContact.notes);
-    }
+    var editIndex = localStorage.getItem('editContactIndex');  // Get the index from local storage
+if (editIndex !== null) {  // If there's an index in local storage, it means we're editing a contact
+    $('#editIndex').val(editIndex);  // Set the hidden input field's value to this index
+    localStorage.removeItem('editContactIndex');  // Clear the index from local storage so it's not mistakenly used later
+}
 
     $('form').submit(function(e) {
         e.preventDefault();
-        var data = $(this).serializeFormJSON();
 
-        if(urlParams.has('edit')) {
-            // If in edit mode, update the existing contact instead of adding new
-            itemsArray[editedContactIndex] = data;
-        } else {
-            itemsArray.push(data);
+        if (!this.checkValidity()) {
+            alert("Input is invalid. Please correct and submit again.");
+            return;
         }
 
-        localStorage.setItem('itemsArray', JSON.stringify(itemsArray));
-
-        $('#submission-msg').fadeIn().delay(3000).fadeOut(function() {
-            window.location.href = "contacts-list.html";
-        });
+        var data = $(this).serializeFormJSON();
+        var profilePictureFile = $('#profilePictureField')[0].files[0];
+        if (profilePictureFile) {
+            var reader = new FileReader();
+            reader.readAsDataURL(profilePictureFile);
+            reader.onload = function() {
+                data.profilePicture = reader.result; // Base64 encoded image string
+                saveContact(data);
+            };
+        } else {
+            data.profilePicture = null;
+            saveContact(data);
+        }
     });
+
+    // Check if the form is in edit mode and load data if necessary
+    var editIndex = $('#editIndex').val();
+    if (editIndex !== undefined && editIndex !== null && editIndex !== '') {
+        var contacts = JSON.parse(localStorage.getItem('itemsArray'));
+        loadContactForEdit(contacts[editIndex]);
+    }
 });
+
+function saveContact(contact) {
+    var oldItems = JSON.parse(localStorage.getItem('itemsArray')) || [];
+    var editIndex = $('#editIndex').val();
+
+    if (editIndex !== undefined && editIndex !== null && editIndex !== '') {
+        oldItems[editIndex] = contact; // Update existing contact
+    } else {
+        oldItems.push(contact); // Add new contact
+    }
+    
+    localStorage.setItem('itemsArray', JSON.stringify(oldItems));
+
+    $('#submission-msg').fadeIn().delay(3000).fadeOut(function() {
+        window.location.href = "contacts-list.html";
+    });
+}
+
+function loadContactForEdit(contact) {
+    $('#name').val(contact.name);
+    $('#phone').val(contact.phone);
+    $('#email').val(contact.email);
+    $('#group').val(contact.group);
+    $('#notes').val(contact.notes);
+    $('#birthday').val(contact.birthday);
+    $('#company').val(contact.company);
+
+    // Optionally, set the photo if it exists
+    if (contact.profilePicture) {
+        $('#profilePicturePreview').attr('src', contact.profilePicture);
+    }
+    // Change the submit button to "Update" or something to indicate it's in edit mode
+    $('form button[type="submit"]').text('Update');
+}
 
 (function($) {
     $.fn.serializeFormJSON = function() {
--- js/table.js
+++ js/table.js
@@ -1,45 +1,57 @@
 var addressBook = (function() {
-var myContacts = JSON.parse(localStorage.getItem('itemsArray')) || [];
-  var table = $('#table1');
-  var tbody = table.find('tbody');
+    var myContacts = JSON.parse(localStorage.getItem('itemsArray'));
+    var table = $('#table1');
+    var tbody = table.find('tbody');
 
-  table.on('click', '#remove', deletePerson);
-  table.on('click', '#edit', editPerson);
-  _render();
+    table.on('click', '#remove', deletePerson);
+    table.on('click', '#edit', editPerson);
+    _render();
 
-  function _render() {
-    tbody.html('');
-    var length = myContacts.length;
-    for (var i = length - 1; i >= 0; i--) {
-        table.prepend('<tr><td>' + (i + 1) + '</td><td>' + myContacts[i].name + '</td><td>' + myContacts[i].email + '</td><td>' + myContacts[i].phone + '</td><td>' + myContacts[i].birthday + '</td><td>' + myContacts[i].company + '</td><td>' + myContacts[i].group + '</td><td>' + myContacts[i].notes + '</td><td><button id="edit" class="btn btn-info">Edit</button></td><td><button id="remove" class="btn btn-danger">Delete</button></td></tr>');
+    function _render() {
+        tbody.html('');
+        var length = myContacts.length;
+        for (var i = length - 1; i >= 0; i--) {
+            var imgHtml = myContacts[i].profilePicture 
+                ? '<img src="' + myContacts[i].profilePicture + '" alt="' + myContacts[i].name + '" width="50" height="50">'
+                : 'No Image';
+            
+            var group = myContacts[i].group || "N/A"; // If 'group' is not defined, use "N/A"
+            var notes = myContacts[i].notes || "N/A"; // If 'notes' is not defined, use "N/A"
+            var birthday = myContacts[i].birthday || "N/A"; // If 'birthday' is not defined, use "N/A"
+            var company = myContacts[i].company || "N/A"; // If 'company' is not defined, use "N/A"
+    
+            table.prepend('<tr><td>' + i + '</td><td>' + imgHtml + '</td><td>' + myContacts[i].name + '</td><td>' + myContacts[i].phone + '</td><td>' + myContacts[i].email + '</td><td>' + group + '</td><td>' + notes + '</td><td>' + birthday + '</td><td>' + company + '</td><td><button id="edit" class="btn btn-info">Edit</button><button id="remove" class="btn btn-danger">X</button></td></tr>');
+        }
     }
-}
+    
+    
+    
 
-  function deletePerson(event) {
-      var i = $('#table1 tr').index($(this).closest('tr')) - 1;
-      myContacts.splice(i, 1);
-      _render();
-      localStorage.clear();
-      localStorage.setItem('itemsArray', JSON.stringify(myContacts));
-  }
+    function deletePerson(event) {
+        var i = $('#table1 tr').index($(this).closest('tr')) - 1;
+        myContacts.splice(i, 1);
+        _render();
+        localStorage.setItem('itemsArray', JSON.stringify(myContacts));
+    }
 
-  function editPerson(event) {
-    var i = $('#table1 tr').index($(this).closest('tr')) - 1;
-    var editedContact = myContacts[i];
-    localStorage.setItem('editedContactIndex', i); // Save the index of the contact being edited
-    window.location.href = "index.html?edit=true"; // Redirect to form page with edit flag
-}
+    function editPerson(event) {
+        var i = $('#table1 tr').index($(this).closest('tr')) - 1;  // Find out which row was clicked
+        localStorage.setItem('editContactIndex', i);  // Store that row number in local storage
+        window.location.href = "index.html";  // Redirect to the contact form page
+    }
+    
+    
 
+    $("#search").on("keyup", function() {
+        var value = $(this).val().toLowerCase();
+        $("table tr").filter(function(index) {
+            if (index > 0) {
+                $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
+            }
+        });
+    });
 
-  $("#search").on("keyup", function() {
-      var value = $(this).val().toLowerCase();
-      $("table tr").filter(function(index) {
-          if (index > 0) {
-              $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
-          }
-      });
-  });
-  return {
-      deletePerson: deletePerson
-  };
+    return {
+        deletePerson: deletePerson
+    };
 })();
