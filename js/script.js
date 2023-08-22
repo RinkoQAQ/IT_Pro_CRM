a68a8e5796e328140cdda446cff29103d939f3b5
RinkoQAQ <lyyan3@student.unimelb.edu.au>
2023-08-22 18:57:30 +1000

    More instruction for backend team

--- js/script.js
+++ js/script.js
@@ -1,6 +1,37 @@
 $(document).ready(function() {
 
-    
+
+    /*这个JavaScript代码使用jQuery库，是一个用于在浏览器中处理表单数据的代码。它旨在将联系人信息保存到localStorage（本地存储），以便以后可以方便地访问这些信息。
+
+以下是这段代码的具体说明：
+
+$(document).ready(function() { ... });：这是一个jQuery语句，用于在DOM文档准备好后执行函数中的代码。
+
+var editIndex = localStorage.getItem('editContactIndex');：这行代码从localStorage中检索名为'editContactIndex'的值（如果存在），并将其保存到变量editIndex中。
+
+if (editIndex !== null) { ... }：此部分代码是一个条件语句，检查editIndex是否为空。如果不为空，说明我们正在编辑一个已有的联系人。
+
+在第3点的代码块内部，我们从localStorage中获取了名为itemsArray的数组（它包含所有联系人信息），并从其中提取了要编辑的联系人的信息。然后我们将该联系人的信息填充到表单中。
+
+有一部分代码处理联系人的照片（如果存在）并将其显示在表单中。
+
+localStorage.removeItem('editContactIndex');：此行代码将移除名为'editContactIndex'的localStorage项目，因为我们已经处理了要编辑的联系人。
+
+$('form').submit(function(e) { ... });：此部分代码绑定一个函数到表单的提交事件。该函数阻止默认的表单提交行为，并处理表单数据。
+
+在提交处理函数中，我们先检查表单是否有效。如果无效，将弹出一个警告框。然后我们收集表单数据，包括联系人的照片，将其转换为数据URL，并将其添加到数据对象中。
+
+function processFormSubmission(data) { ... }：此函数处理表单数据。它首先从localStorage中检索名为'itemsArray'的数组（包含所有联系人信息），或者如果不存在该数组，则创建一个空数组。然后它会更新或添加新联系人信息，并将修改后的数组保存回localStorage。
+
+最后，我们显示一个确认消息，然后将用户重定向到联系人列表页面。
+
+(function($) { ... })(jQuery);：此代码块定义了一个名为serializeFormJSON的jQuery插件，该插件将表单数据序列化为JSON对象。
+
+总体而言，这段代码的目的是允许用户添加或编辑联系人信息，并将这些信息保存到本地存储中，以便以后访问。*/    
+
+
+
+
     // Check if in edit mode and populate form with existing data
     var editIndex = localStorage.getItem('editContactIndex');
     if (editIndex !== null) {
--- js/table.js
+++ js/table.js
@@ -1,3 +1,38 @@
+/*这段JavaScript代码定义了一个名为addressBook的对象，该对象是一个立即执行的函数表达式（IIFE）。该对象主要用于处理和显示联系人信息，并提供了用于删除和编辑联系人信息的功能。
+
+以下是这段代码的主要功能和结构：
+
+在IIFE内部，我们首先从localStorage中获取名为'itemsArray'的联系人信息数组，并保存到变量myContacts中。
+
+我们选择名为#table1的HTML表格，并保存对其tbody元素的引用。
+
+我们为删除和编辑按钮添加点击事件处理程序。
+
+我们调用_render函数，该函数将从myContacts数组中的联系人信息渲染到表格中。
+
+_render函数：
+
+首先清除表格的内容。
+然后遍历联系人数组，并逐个添加联系人信息到表格中。每个联系人的行包括序号、图片、姓名、电话、电子邮件、分组、备注、生日、公司和操作按钮（编辑和删除）。
+对于联系人的分组、备注、生日和公司，如果没有定义，则显示"N/A"。
+deletePerson函数：
+
+通过点击的删除按钮找到对应的表格行索引。
+使用splice方法从myContacts数组中删除对应的联系人。
+调用_render函数重新渲染表格。
+将修改后的myContacts数组保存到localStorage中。
+editPerson函数：
+
+通过点击的编辑按钮找到对应的表格行索引。
+将该行索引保存到localStorage中（名为'editContactIndex'）。
+重定向到联系人表单页面。
+我们为搜索框添加keyup事件处理程序，该处理程序允许用户在输入搜索词时实时过滤联系人表格。
+
+最后，我们将deletePerson函数暴露为addressBook对象的公共方法。
+
+总体而言，该代码的主要功能是从localStorage中检索联系人信息，显示这些信息在一个HTML表格中，并允许用户编辑、删除和搜索联系人。*/
+
+
 var addressBook = (function() {
     var myContacts = JSON.parse(localStorage.getItem('itemsArray'));
     var table = $('#table1');
