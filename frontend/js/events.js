b102ccb7218cc14390c9b0a2a4ba78e95906597d
Jiashengx1 <jiashengxu7@gmail.com>
2023-09-21 21:19:36 +1000

    test deploy fullcalendar

--- frontend/js/events.js
+++ frontend/js/events.js
@@ -1,28 +1,32 @@
-$(document).ready(function() {
-    const API_ENDPOINT = 'https://personalcrmbackend-042e5db40ee3.herokuapp.com/events'; // 替换为你的后端API地址
+var eventManager = (function() {
+    const API_ENDPOINT = 'https://personalcrmbackend-042e5db40ee3.herokuapp.com/events';  // Replace with your API Endpoint
     const calendarElement = $('#calendar');
 
-    function renderEvents() {
+    $(document).ready(function() {
+        initCalendar();
+        fetchAndRenderEvents();
+    });
+
+    function initCalendar() {
+        calendarElement.fullCalendar({
+            // Your FullCalendar options here
+        });
+    }
+
+    function fetchAndRenderEvents() {
         fetch(API_ENDPOINT)
             .then(response => response.json())
             .then(events => {
-                const formattedEvents = events.map(event => ({
-                    title: event.content,
-                    start: event.time,
-                    allDay: false
-                }));
-                
-                // 初始化FullCalendar
-                calendarElement.fullCalendar({
-                    events: formattedEvents
-                });
+                calendarElement.fullCalendar('removeEvents');
+                calendarElement.fullCalendar('addEventSource', events);
             })
             .catch(error => {
-                console.error("Error fetching events:", error);
+                console.error('Error fetching events:', error);
             });
     }
 
-    // 当页面加载完成后调用此函数
-    window.onload = renderEvents;
-
+    return {
+        initCalendar: initCalendar,
+        fetchAndRenderEvents: fetchAndRenderEvents
+    };
 })();
