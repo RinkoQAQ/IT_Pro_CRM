26b5c41badf4a8312ef428c53127aa70fc9da533
Jiashengx1 <jiashengxu7@gmail.com>
2023-09-21 22:03:24 +1000

    replace fullcalendar to DHTMLX Scheduler

--- frontend/js/events.js
+++ frontend/js/events.js
@@ -1,22 +1,23 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const API_ENDPOINT = 'https://personalcrmbackend-042e5db40ee3.herokuapp.com/events';
-    
-    var calendarEl = document.getElementById('calendar');
-  
-    var calendar = new FullCalendar.Calendar(calendarEl, {
-      initialView: 'dayGridMonth',
-      events: function(fetchInfo, successCallback, failureCallback) {
-        fetch(API_ENDPOINT)
-          .then(res => res.json())
-          .then(events => {
-            successCallback(events);
-          })
-          .catch(err => {
-            failureCallback(err);
-          });
-      }
-    });
-  
-    calendar.render();
-  });
-  
+    scheduler.config.xml_date = "%Y-%m-%d %H:%i";
+    scheduler.init('scheduler_here', new Date(), "month");
+
+    const API_ENDPOINT = 'https://personalcrmbackend-042e5db40ee3.herokuapp.com';
+
+    fetch(`${API_ENDPOINT}/events`)
+        .then(response => response.json())
+        .then(data => {
+            const schedulerEvents = data.map(event => {
+                return {
+                    start_date: new Date(event.time),
+                    end_date: new Date(new Date(event.time).getTime() + 60 * 60 * 1000), // 假设事件时长为1小时
+                    text: event.content + ' (' + event.customerName + ')'
+                };
+            });
+
+            scheduler.parse(schedulerEvents, "json");
+        })
+        .catch(error => {
+            console.error('Error fetching events:', error);
+        });
+});
